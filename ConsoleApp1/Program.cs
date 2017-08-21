531973ebc16cde122430ada872626a56ed7261d7
プラジュワル ニンマガッダ <prajcindav@outlook.com>
2017-08-21 16:56:06 -0300

    FUlly functional OCR Sample

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tesseract;
 
 namespace ConsoleApp1
 {
@@ -10,6 +7,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            
+                Console.Title = "Trying Tesseract";
+
+                const string tessDataDir = @"C:\Users\prajwal.nimmagadda\Desktop\Automation GitHub\tessdata";
+                const string imageDir = @"C:\Users\prajwal.nimmagadda\Desktop\Automation GitHub\Test5.jpg";
+
+                using (var engine = new TesseractEngine(tessDataDir, "eng", EngineMode.Default))
+                using (var image = Pix.LoadFromFile(imageDir))
+                using (var page = engine.Process(image))
+                {
+                    string text = page.GetText();
+                    Console.WriteLine(text);
+                    Console.ReadLine();
+                }
         }
     }
 }
